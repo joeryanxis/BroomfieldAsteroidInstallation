d38b86e261bff381bfd7d18f26bd04b197e28b40
samuraiversace <37460306+samuraiversace@users.noreply.github.com>
2019-11-22 14:50:40 -0700

    Initial upload

---
+++ Project/API Testing/sketch.js
@@ -0,0 +1,22 @@
+var startDate, endDate, meteors;
+
+startDate = '2019-10-22';
+endDate = '2019-10-28';
+function setup(){
+	createCanvas(windowWidth,windowHeight);
+	loadJSON('https://api.nasa.gov/neo/rest/v1/feed?start_date='+startDate+'&end_date='+endDate+'&api_key=XCwfaiBZBeLcUErNU0XHGRUyc7mmqu1LhzIxNt0Q',visualization)
+	textSize(32);
+}
+
+function visualization(data){
+	//background(222);
+	meteors = data
+	console.log(meteors);
+	console.log(meteors.near-earth-objects);
+}
+
+function draw(){
+	background(100);
+	
+	
+}
